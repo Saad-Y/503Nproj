2f0e071d3969cb71018fc7dfe3f48604fd06bf37
Aya Al Baba <ayababa12@outlook.com>
2025-04-08 09:10:30 +0300

    database setup and created user schema

--- eep/app.py
+++ eep/app.py
@@ -4,6 +4,8 @@ import openai
 from azure.identity import DefaultAzureCredential
 from azure.keyvault.secrets import SecretClient
 from document_upload import document_upload_route
+from database.database import db
+import os
 
 VAULT_URL = "https://vault503n.vault.azure.net/"
 credential = DefaultAzureCredential()
@@ -14,6 +16,25 @@ app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 app.register_blueprint(document_upload_route)
 
+# Connect to db
+mysql_password = client.get_secret("DB-PASSWORD").value
+ssl_cert = client.get_secret("DigiCert-CA-Cert").value
+cert = "-----BEGIN CERTIFICATE-----\n" + '\n'.join([ssl_cert[i:i+64] for i in range(0, len(ssl_cert), 64)]) + "\n-----END CERTIFICATE-----"
+os.makedirs('tmp', exist_ok=True)
+cert_path = "./tmp/DigiCertGlobalRootCA.crt.pem"
+with open(cert_path, "w") as f:
+    f.write(cert)
+app.config['SQLALCHEMY_DATABASE_URI'] = (
+    f'mysql+pymysql://learnify:{mysql_password}@learnifysqldb.mysql.database.azure.com:3306/learnifydb?'
+    f'ssl_ca={cert_path}'
+)
+db.init_app(app)
+
+with app.app_context():
+    from model.user import User
+
+    if app.config["SQLALCHEMY_DATABASE_URI"]:
+        db.create_all()
 
 
 if __name__ == '__main__':
