45c7035078c8ecf55262cb1ff0219d9a2ff18384
Aya Al Baba <ayababa12@outlook.com>
2025-04-25 17:44:38 +0300

    defaulted some env vars (db pass and cert)

--- eep/secrets.py
+++ eep/secrets.py
@@ -2,6 +2,6 @@ import os
 from dotenv import load_dotenv
 
 OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
-mysql_password = os.getenv("DB_PASSWORD")
-ssl_cert = os.getenv("DigiCert_CA_Cert")
+mysql_password = os.getenv("DB_PASSWORD", "")
+ssl_cert = os.getenv("DigiCert_CA_Cert", "")
 SECRET_KEY = os.getenv('SECRET_KEY')
