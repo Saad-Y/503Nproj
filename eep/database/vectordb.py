5c910c3c02b093cbd03ef2a139d410a690d26a6b
Aya Al Baba <ayababa12@outlook.com>
2025-04-25 17:23:27 +0300

    local conf for databases

--- eep/database/vectordb.py
+++ eep/database/vectordb.py
@@ -1,3 +1,11 @@
 from chromadb import HttpClient
+from chromadb import PersistentClient
+import os
 
-client = HttpClient(host='74.243.233.220', port=8000)
+LOCAL = os.getenv("LOCAL", "false")
+
+if LOCAL == "false":    
+    client = HttpClient(host='74.243.233.220', port=8000)
+
+else:
+    client = PersistentClient(path="./")
